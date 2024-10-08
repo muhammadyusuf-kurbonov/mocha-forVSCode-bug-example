00beccc8e11ae46352503061ac653bffbc904ea7
Muhammadyusuf Kurbonov <muhammadyusuf.kurbonov2002@gmail.com>
2024-10-08 20:39:31 +0500

    report error about state

--- test/example.test.ts
+++ test/example.test.ts
@@ -1,5 +1,12 @@
 describe('Example test', () => {
+
+    before(() => {
+        throw new Error('before');
+    });
+
     it('test example', () => {
         console.log('Started and passed');
     });
+    it.skip('Skipped test', () => {});
+    it('empty test');
 });
